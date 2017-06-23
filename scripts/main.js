962b9364607a3c6c4aaa7db56f5b3fb0101d49aa
Peter Brandon <peterkbrandon@gmail.com>
2017-06-23 15:59:15 -0400

    First commit

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/jon.jpg') {
+      myImage.setAttribute ('src','images/luke.jpg');
+    } else {
+      myImage.setAttribute ('src','images/jon.jpg');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Welcome to the UConn Outing Club, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Welcome to the UConn Outing Club, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
